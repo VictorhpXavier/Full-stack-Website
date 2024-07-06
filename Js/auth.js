f5399bb37999065f370abd7924b9be32a37dc01b
VictorhpXavier <victorhpxavier.09@gmail.com>
2024-07-06 19:46:51 +0000

    Not working

--- Js/auth.js
+++ Js/auth.js
@@ -23,10 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "My!Super(16)Secret#(05)Password$",
-    database: "VHX_DataBase"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 con.connect((err) => {
     if (err) {
